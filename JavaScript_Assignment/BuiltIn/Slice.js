bcaae64524fde32084de2fa548662a8faa770ab7
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-10-13 18:41:09 +0530

    Create Slice.js

---
+++ JavaScript_Assignment/BuiltIn/Slice.js
@@ -0,0 +1,8 @@
+var animals = ["cat", "dog", "rat", "lion"];
+var x = animals.slice(1,3);
+console.log(x);
+console.log(animals);
+var animals = ["cat", "dog", "rat", "lion"];
+var manu = animals.splice(2);
+console.log("Manu :",manu);
+console.log("Original",animals);
