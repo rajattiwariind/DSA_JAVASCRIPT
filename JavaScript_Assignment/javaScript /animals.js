c768b924357020132e0a57b6da9bbf5ae28f5039
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-10-13 17:34:12 +0530

    Create animals.js

---
+++ JavaScript_Assignment/javaScript /animals.js
@@ -0,0 +1,6 @@
+const animals = ['pigs', 'goats', 'sheep'];
+const count = animals.push('cows');
+console.log(count);
+console.log(animals);
+animals.push('chickens', 'cats', 'dogs');
+console.log(animals);
