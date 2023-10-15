30d6de010d39379323e26f31c949c2d22747f5c3
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-10-15 19:42:07 +0530

    Create Data2.js

---
+++ JavaScript_Assignment/Object/Data2.js
@@ -0,0 +1,12 @@
+var data2 = {
+      name : "Kaleen Bhaiyya",
+      age : 45,
+      gender : "male",
+      city : "Mirzapur",
+      hobbies : ["Making Guns"]
+};
+
+for(var key in data2)
+{
+  console.log(key," --- ",data2[key]);
+}
