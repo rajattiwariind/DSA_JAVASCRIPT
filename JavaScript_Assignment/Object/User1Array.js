1e9835978ccd1e001dec0d173995b376df9c2983
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-10-15 19:45:50 +0530

    Create User1Array.js

---
+++ JavaScript_Assignment/Object/User1Array.js
@@ -0,0 +1,12 @@
+// Arrays
+	var user1 = ["Rahul", 25, "male", "Bangalore", "coding"];
+
+// Objects
+	var user2 = {
+		name : "Rahul",
+		age  : 25,
+		gender: "male",
+		city  : "Bangalore",
+		hobbies: "coding"
+};
+console.log(user2)
