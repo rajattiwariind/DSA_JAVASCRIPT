e0bb2eed9dcbd3d962ef146a1fe564899422b731
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-10-25 20:52:04 +0530

    Create Array5.js

---
+++ JavaScript_Assignment/MultiDimArray/Array5.js
@@ -0,0 +1,7 @@
+var iitems = [];
+
+var bag1 = ["1 Banana", "1 Potato", ["Onion_1", "Onion_2"]];
+var bag2 = ["1 Brinjal", "1 Coconut", "Brush"];
+
+console.log(bag1.length);
+console.log(bag2.length);
