8ca7250b2adb645465945466d04f8741292c7203
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2023-10-25 20:52:47 +0530

    Create Array6.js

---
+++ JavaScript_Assignment/MultiDimArray/Array6.js
@@ -0,0 +1,16 @@
+var items = [
+			    ["vatsal", 28, "male"],
+				["Hitesh", 30, "male"]
+			];
+
+// Print Names
+console.log(items[0][0]);
+console.log(items[1][0]);
+
+// Print Roll No of Both
+console.log(items[0][1]);
+console.log(items[1][1]);
+
+// Print gender of both
+console.log(items[0][2]);
+console.log(items[1][2]);
