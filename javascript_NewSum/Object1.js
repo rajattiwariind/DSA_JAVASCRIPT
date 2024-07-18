79875c244955d5523d7af47241817cb24bcbcbd9
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2024-07-18 20:19:03 +0530

    Create Object1.js

---
+++ javascript_NewSum/Object1.js
@@ -0,0 +1,19 @@
+var student = {
+  name : "lalu",
+  grade : "X",
+  section : "A",
+  maths : 30,
+  science : 40,
+  english : 35,
+  geography : 50,
+  history : 80,
+  biology : 20
+}
+
+console.log(student.name);
+console.log(student.grade);
+console.log(student.section);
+
+console.log(student.maths);
+console.log(student.science);
+console.log(student.english);
