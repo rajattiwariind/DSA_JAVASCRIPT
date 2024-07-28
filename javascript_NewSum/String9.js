0aa077936ce8df30e4884067eb4c70ffb167ac18
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2024-07-28 18:59:05 +0530

    Create String9.js

---
+++ javascript_NewSum/String9.js
@@ -0,0 +1,18 @@
+**I Way**
+
+var name = "Masai";
+var name2 = []
+
+for(var i=0; i<name.length; i++)
+{
+	name2.push(name[i]);
+}
+
+name2[0] = "N";
+var bag = "";
+for(var i=0; i<name2.length; i++)
+{
+	bag = bag + name2[i];
+}
+console.log(bag);
+
