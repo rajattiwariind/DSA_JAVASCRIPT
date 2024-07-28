c99254a10bac1440ebcb1e4c80f961d7b5ebd72d
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2024-07-28 18:48:31 +0530

    Create String1.js

---
+++ javascript_NewSum/String1.js
@@ -0,0 +1,10 @@
+var name = "Masai";
+console.log(name);
+
+console.log(name[0]);   // M
+console.log(name[1]);   // a
+console.log(name[2]);   // s
+console.log(name[3]);   // a
+console.log(name[4]);   // i
+console.log(name[5]);   // undefined
+
