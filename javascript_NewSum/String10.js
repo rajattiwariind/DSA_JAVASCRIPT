255510ad4f48676e0ab1844051551098f3c31697
Rajat Tiwari <129544468+rajattiwariind@users.noreply.github.com>
2024-07-28 18:59:34 +0530

    Create String10.js

---
+++ javascript_NewSum/String10.js
@@ -0,0 +1,19 @@
+**II Way**
+
+var name = "Masai";
+var output = "";
+
+for(var i=0; i<name.length; i++)
+{
+	if(i==0)
+	{
+		output = output + "N";
+	}
+	else
+	{
+		output = output + name[i];
+	}
+}
+
+console.log(output);     // Nasai
+
